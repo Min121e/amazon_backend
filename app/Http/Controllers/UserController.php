0dc99b847763319d17de0aa19cc753e2a2b3e92d
Min121e <97957887+Min121e@users.noreply.github.com>
2023-12-15 09:56:26 +0630

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,8 +45,6 @@ class UserController extends Controller
     
         if (Auth::attempt($credentials)) {
             $user = User::where('email', $credentials['email'])->first();
-            // $user = auth()->user;
-            // $user = Auth::user()->id;
             if ($user && Hash::check($credentials['password'], $user->password)) {
                 $token = $request->user()->createToken('api_token')->plainTextToken;
                 return response()->json(['user' => $user, 'token' => $token]);
@@ -59,22 +57,16 @@ class UserController extends Controller
     // public function signout(Request $request) {
         // $request->auth()->user()->id->tokens()->delete();
         // return auth()->user;
+    
         // $request->user()->currentAccessToken()->delete();
 
         // $request->user()->tokens()->delete();
+    
         // return response()->json(['message' => 'Logged Out'], 200);
-        
-        // return Auth::logout();
-
 
         // 2.
-        // Get bearer token from the request
         // $accessToken = $request->bearerToken();
-        
-        // // Get access token from database
         // $token = PersonalAccessToken::findToken($accessToken);
-
-        // // Revoke token
         // $token->delete();
     // }
 
@@ -88,110 +80,12 @@ class UserController extends Controller
         }
     }
 
-    // public function signin(Request $request) {
-    //     $credentials = $request->validate([
-    //         'email' => 'required',
-    //         'password' => 'required'
-    //     ]);
-    
-    //     // Attempt to authenticate the user
-    //     if (Auth::attempt($credentials)) {
-    //         // If authentication is successful, you can retrieve the authenticated user using the Auth facade
-    //         $user = Auth::user();
-    //         $token = $user->createToken('api-token')->plainTextToken;
-    
-    //         return response()->json(['user' => $user, 'token' => $token]);
-    //     }
-    
-    //     return response()->json(['error' => 'Email or password is incorrect'], 401);
-    // }
-    
-    
-    
-
-
-
-   
-
 
 
     public function loggedinuser() {
         return Auth::user();
     }
     
-
-
-    
-
-    
-
-
-    public function userinfo($id) {
-        if (auth()->check()) {
-            $user = User::find($id);
-    
-            if (!$user) {
-                return response()->json(['error' => 'User not found'], 404);
-            }
-    
-            return response()->json(['user' => $user]);
-        } else {
-            return response()->json(['error' => 'User not haha authenticated'], 401); // Use 401 for unauthenticated access.
-        }
-        return response()->json(['error' => 'User not haha authenticated'], 401);
-    }
-    
-
-    // public function userinfo($id) {
-    //     if(Auth::id()) {
-    //         $user = User::find($id);
-    //         // $user = User::all();
-    //         return $user;
-    //     } else {
-    //         return response()->json(['error' => 'User is not authenticated'], 401);
-    //     }
-    // }
-
-
-    // public function signin(Request $request) {
-    //     $credentials = $request->validate([
-    //         'email' => 'required',
-    //         'password' => 'required'
-    //     ]);
-    
-    //     if (Auth::attempt($credentials)) {
-    //         $user = User::where('email', $credentials['email'])->first();
-    //         if ($user && Hash::check($credentials['password'], $user->password)) {
-    //             $token = $user->createToken('api-token')->plainTextToken;
-    //             $cookie = cookie('api_token', $token, 60*24); //name, token variable, validity duration
-    //             return response(['user' => $user])->withCookie($cookie);
-    //         }
-    //     }
-    //     return response()->json(['error' => 'Email or password is incorrect'], 401);
-    // }
-
-
-
-    // public function addToFavourite(Request $request)
-    // {
-    //     $request->validate([
-    //         'user_id' => 'required|exists:users,id',
-    //         'product_id' => 'required|exists:products,id',
-    //     ]);
-
-    //     // Check if the user already has this product in favorites
-    //     if (!$request->user()->favourites()->where('product_id', $request->product_id)->exists()) {
-    //         // If not, attach the product to the user's favorites
-    //         $request->user()->favourites()->attach($request->product_id);
-            
-    //         // You can return a success response here if needed
-    //         return response()->json(['message' => 'Product added to favourites successfully']);
-    //     }
-
-    //     // You can return a response here indicating that the product is already in favorites
-    //     return response()->json(['message' => 'Product already in favourites']);
-    // }
-
 
     public function addToFavourite(Request $request)
     {
